2f540800f9fb2908d24201c939cfd38194fa0548
Aditya Fakhri Riansyah <adityafakhri03@gmail.com>
2025-01-20 16:27:27 +0700

    fix: layout master add yield section

--- resources/views/admin/layouts/__sidebar.blade.php
+++ resources/views/admin/layouts/__sidebar.blade.php
@@ -45,43 +45,43 @@
             <ul class="menu">
                 <li class="sidebar-title">Menu</li>
 
-                <li class="sidebar-item active ">
-                    <a href="index.html" class='sidebar-link'>
+                <li class="sidebar-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
+                    <a href="{{ route('dashboard') }}" class='sidebar-link'>
                         <i class="bi bi-grid-fill"></i>
                         <span>Dashboard</span>
                     </a>
                 </li>
 
-                <li class="sidebar-item">
-                    <a href="menu-management.html" class='sidebar-link'>
+                <li class="sidebar-item {{ request()->routeIs('items.*') ? 'active' : '' }}">
+                    <a href="{{ route('items.index') }}" class='sidebar-link'>
                         <i class="bi bi-file-earmark-text-fill"></i>
                         <span>Daftar Menu</span>
                     </a>
                 </li>
 
-                <li class="sidebar-item">
-                    <a href="order-management.html" class='sidebar-link'>
+                <li class="sidebar-item {{ request()->routeIs('orders.*') ? 'active' : '' }}">
+                    <a href="{{ route('orders.index') }}" class='sidebar-link'>
                         <i class="bi bi-cart-fill"></i>
                         <span>Kelola Pesanan</span>
                     </a>
                 </li>
 
-                <li class="sidebar-item">
-                    <a href="user-management.html" class='sidebar-link'>
+                <li class="sidebar-item {{ request()->routeIs('users.*') ? 'active' : '' }}">
+                    <a href="{{ route('users.index') }}" class='sidebar-link'>
                         <i class="bi bi-person-fill"></i>
                         <span>Manajemen Karyawan</span>
                     </a>
                 </li>
 
-                <li class="sidebar-item">
-                    <a href="user-management.html" class='sidebar-link'>
+                <li class="sidebar-item {{ request()->routeIs('roles.*') ? 'active' : '' }}">
+                    <a href="{{ route('roles.index') }}" class='sidebar-link'>
                         <i class="bi bi-person-fill-gear"></i>
                         <span>Manajemen Role</span>
                     </a>
                 </li>
 
-                <li class="sidebar-item">
-                    <a href="user-management.html" class='sidebar-link'>
+                <li class="sidebar-item {{ request()->routeIs('categories.*') ? 'active' : '' }}">
+                    <a href="{{ route('categories.index') }}" class='sidebar-link'>
                         <i class="bi bi-tags-fill"></i>
                         <span>Manajemen Kategori</span>
                     </a>
