514ecd5bd5cde75cd6d871231380ec51f100638a
Aditya Fakhri Riansyah <adityafakhri03@gmail.com>
2025-01-20 16:28:28 +0700

    feat: add routes for all link

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
-    return view('admin.layouts.master');
-});
+    return view('admin.dashboard');
+})->name('dashboard');
+
+Route::resource('items', ItemController::class);
+Route::resource('orders', OrderController::class);
+Route::resource('users', UserController::class);
+Route::resource('roles', RoleController::class);
+Route::resource('categories', CategoryController::class);
