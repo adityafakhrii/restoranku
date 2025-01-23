6038c0ee7d5031dbfca765789cd8fc95bc1783d5
Aditya Fakhri Riansyah <adityafakhri03@gmail.com>
2025-01-23 21:21:49 +0700

    feat: add cancel button on create item

--- resources/views/admin/item/create.blade.php
+++ resources/views/admin/item/create.blade.php
@@ -9,11 +9,23 @@
 @endsection
 
 @section('content')
-<div class="card">
-
-    <div class="card-header">
-        <h4 class="card-title">Tambah Data Menu</h4>
+<div class="page-title">
+    <div class="row mb-3">
+        <div class="col-lg-9 col-sm-12">
+            <h3>Tambah Data Menu</h3>
+            <p class="text-subtitle text-muted">Silakan isi form dengan benar</p>
+        </div>
+        <div class="col-lg-3 col-sm-12">
+            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ url('items.index') }}">Daftar Menu</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Tambah Data Menu</li>
+                </ol>
+            </nav>
+        </div>
     </div>
+</div>
+<div class="card">
     <div class="card-content">
 
         <div class="card-body">
@@ -99,6 +111,7 @@
                         <div class="col-12 d-flex justify-content-end">
                             <button type="submit" class="btn btn-primary me-1 mb-1">Simpan</button>
                             <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                            <a href="{{ route('items.index') }}" class="btn btn-light-secondary me-1 mb-1">Batal</a>
                         </div>
                     </div>
                 </div>
